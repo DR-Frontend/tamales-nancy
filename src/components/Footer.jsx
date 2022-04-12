12f806d220288d9e94b96951a00df296753119ad
David Romero <dvdrmrrdz@gmail.com>
2022-04-11 20:43:37 -0500

    main completo y ajustes en footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,7 +1,9 @@
 const Footer = ()=>(
    <footer id="footer" className="footer">
       <div className="wrapp">
-         <h2>Footer</h2>
+         <h2>¿LISTO PARA HACER SU PEDIDO?</h2>
+         <p>Con gusto atenderemos sus pedidos para sus eventos. ¿Día de la Candelaria?, ¿Posadas de Diciembre? o ¿debe alguna apuesta?, no se preocupe, Los tamales de 'La Madrina Nancy' entrarán al rescate!.</p><br/>
+         <a href="mailto:tamales.madrina.nancy@gmail.com" target="_blank" className="btn">Hacer un Pedido</a>
       </div>
    </footer>
 )
--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,25 +1,109 @@
-
-
 const Main = ()=>(
    <main id="main" className="main">
       <div className="main__lists wrapp">
-         <h2>TAMALES DE CAZUELA</h2>
-         <p>¡La 'carta fuerte' de la casa!</p>
+         <h2>TAMALES DE 'CAZUELA'</h2>
+         <p>¿Por qué de Cazuela? Así es llamado el estilo con el que nuestra querida 'Madrina Nancy' prepara sus tamales. La receta secreta con la que niños y adultos se han deleitado con estos tamales desde 1972 hasta la actualidad.</p>
          <div className="main__lists-tamales">
             <div className="tamal">
                <div className="tamal__txt">
                   <h2>Champiñones</h2>
-                  <p>Tamal verde con relleno de carne de cerdo</p><br/>
-                  <a href="#" className="btn">Hacer un pedido</a>
+                  <p><span>Para los Aventureros.</span></p><br/>
+                  <p>Relleno con champiñones y queso</p>
                </div>
                <div className="tamal__img">
-                  <img src="https://placeimg.com/150/200/animal" alt="" />
+                  <img src="https://placeimg.com/125/200/animals" alt="" />
+               </div>
+            </div>
+            <div className="tamal">
+               <div className="tamal__txt">
+                  <h2>Verdes</h2>
+                  <p><span>Para los Clásicos.</span></p><br/>
+                  <p>Rellenos con carne de cerdo y salsa verde.</p>
+               </div>
+               <div className="tamal__img">
+                  <img src="https://placeimg.com/125/200/animals" alt="" />
+               </div>
+            </div>
+            <div className="tamal">
+               <div className="tamal__txt">
+                  <h2>Rajas</h2>
+                  <p><span>Para los Valientes.</span></p><br/>
+                  <p>Rellenos con unas picositas rajas y queso.</p>
+               </div>
+               <div className="tamal__img">
+                  <img src="https://placeimg.com/125/200/animals" alt="" />
+               </div>
+            </div>
+            <div className="tamal">
+               <div className="tamal__txt">
+                  <h2>Mole</h2>
+                  <p><span>Para los Variados.</span></p><br/>
+                  <p>Rellenos de carne de cerdo con salsa de guajillo.</p>
+               </div>
+               <div className="tamal__img">
+                  <img src="https://placeimg.com/125/200/animals" alt="" />
+               </div>
+            </div>
+            <div className="tamal">
+               <div className="tamal__txt">
+                  <h2>Dulce rojo/piña</h2>
+                  <p><span>Para los Dulces.</span></p><br/>
+                  <p>Rellenos de pasas o piña. Perfectos como "postre".</p>
+               </div>
+               <div className="tamal__img">
+                  <img src="https://placeimg.com/125/200/animals" alt="" />
+               </div>
+            </div>
+            <div className="tamal">
+               <div className="tamal__txt">
+                  <h2>A la medida</h2>
+                  <p><span>Para los Exploradores.</span></p><br/>
+                  <p>¿Deseas algo nuevo? Hacemos el tamal que tú desees: Zarzamora, Carlos V, Philadephia, etc. </p>
+               </div>
+               <div className="tamal__img">
+                  <img src="https://placeimg.com/125/200/animals" alt="" />
                </div>
             </div>
          </div>
          <h2>COMIDA PARA LLEVAR</h2>
          <p>El 'extra' para deleitar el paladar.</p>
-         <div className="main__lists-foods"></div>
+         <div className="main__lists-foods">
+            <ul className="main__lists-ul">
+               <li className="main__lists-li">
+                  <h2>Atoles</h2>
+                  <p><span>Para que "resbale" todo.</span></p>
+                  <p>Exquisita variedad de atoles para acompañar sus tamales: Cajeta, Piloncillo, Champurrado, Arroz y Fresa.</p>
+               </li>
+               <li className="main__lists-li">
+                  <h2>Tortas</h2>
+                  <p><span>Para tener "energía" todo el día.</span></p>
+                  <p>Deliciosas y calientitas tortas de Cochinita, Choriqueso, Pollo, Jamón, Salchicha, Huevo o Milaneza.</p>
+               </li>
+               <li className="main__lists-li">
+                  <h2>Chilaquiles</h2>
+                  <p><span>Para empezar "bravo" el día.</span></p>
+                  <p>Exquisita variedad de atoles para acompañar sus tamales: Cajeta, Piloncillo, Champurrado, Arroz y Fresa.</p>
+               </li>
+               <li className="main__lists-li">
+                  <h2>Huevos al gusto</h2>
+                  <p><span>Para que gane "fuerzas".</span></p>
+                  <p>Con jamón, salchicha, tocino, con chorizo o a la mexicana. El complemento perfecto para iniciar el día.</p>
+               </li>
+               <li className="main__lists-li">
+                  <h2>Sincronizadas</h2>
+                  <p><span>Para los que comen "ligero".</span></p>
+                  <p>Con jamón, queso oaxaca y salsa verde/roja. Pedirás más de 1 orden.</p>
+               </li>
+               <li className="main__lists-li">
+                  <h2>Tacos Dorados</h2>
+                  <p><span>Para recargar "el tanque".</span></p>
+                  <p>Picadillo, pollo, queso, jamón o papa con queso; y su respectiva crema, lechuga, queso y salsa verde/roja.</p>
+               </li>
+            </ul>
+         </div>
+         <div class="main__video">
+            <iframe width="560" height="315" src="https://www.youtube.com/embed/dup9E3wMdC0" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+         </div>
       </div>
    </main>
 )
